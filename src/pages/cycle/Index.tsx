b6dafa5541fbde3f81992b33efa6573e3d62f67d
JiangHao <978561120@qq.com>
2020-06-24 19:45:57 +0800

    index

---
+++ src/pages/cycle/Index.tsx
@@ -0,0 +1,31 @@
+import React, { useState, useEffect, ChangeEvent } from 'react';
+
+const Person = () => {
+  const [name, setName] = useState('小米');
+
+  useEffect(() => {
+    console.log('组件挂载后要做的操作');
+    return () => {
+      console.log('组件卸载要做的操作');
+    };
+  }, []);
+
+  useEffect(() => {
+    console.log('组件更新后的操作');
+  }, [name]);
+
+  return (
+    <div>
+      <p>欢迎 {name}</p>
+      <input
+        type="text"
+        placeholder="input a username"
+        onChange={(event: ChangeEvent<HTMLInputElement>) =>
+          setName(event.target.value)
+        }
+      />
+    </div>
+  );
+};
+
+export default Person;
