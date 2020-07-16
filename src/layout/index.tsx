daca8c7af656c0f60e8660c6cdeede17f5cbb1e7
jianghao <jianghao@haplox.com>
2020-07-16 22:50:07 +0800

    layout 调整

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -3,39 +3,45 @@ import { IRouteComponentProps, connect, Dispatch } from 'umi';
 import { LoginType } from '@/models/login';
 
 interface PropsType {
-  dispatch: Dispatch;
-  login: LoginType;
-  loading: boolean;
+    dispatch: Dispatch;
+    login: LoginType;
+    loading: boolean;
 }
 
 const layout = ({
-  children,
-  location,
-  route,
-  history,
-  match,
-  dispatch,
-  login,
+    children,
+    location,
+    route,
+    history,
+    match,
+    dispatch,
+    login,
 }: IRouteComponentProps) => {
-  useEffect(() => {
-    // const { dispatch } = props;
-    const token = localStorage.getItem('token');
-    if (token) {
-      dispatch({
-        type: 'login/userMe',
-      });
-    }
-    return () => {
-      console.log(2222233);
-    };
-  }, []);
-  return (
-    <>
-      <div>{login.username && login.username.name}</div>;<div>{children}</div>
-    </>
-  );
+    useEffect(() => {
+        // const { dispatch } = props;
+        const token = localStorage.getItem('token');
+        if (token) {
+            dispatch({
+                type: 'login/userMe',
+            });
+        } else {
+            history.push({
+                pathname: '/login',
+                redirect: match.path,
+            });
+        }
+        return () => {
+            console.log(2222233);
+        };
+    }, []);
+    return (
+        <>
+            <div>{login.username && login.username.name}</div>;
+            <div>{children}</div>
+        </>
+    );
 };
 
 export default connect(({ login }: { login: LoginType }) => ({ login }))(
-  layout,
+    layout,
 );
